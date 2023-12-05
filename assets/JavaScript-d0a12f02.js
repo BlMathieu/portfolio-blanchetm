a0356bc1c0fcfbce8e23a364d7df3dda8c0041f8
Blanchetm <blanchet.mathieu30@gmail.com>
2023-12-06 00:50:47 +0100

    init

---
+++ assets/JavaScript-d0a12f02.js
@@ -0,0 +1,1 @@
+const r=""+new URL("JavaScript-7648f516.png",import.meta.url).href;export{r as _};
---
+++ assets/cssLogo-9f7e8903.js
@@ -0,0 +1,1 @@
+const o=""+new URL("htmlLogo-281ad198.svg",import.meta.url).href,e=""+new URL("cssLogo-6ee9b0ce.png",import.meta.url).href;export{o as _,e as a};
